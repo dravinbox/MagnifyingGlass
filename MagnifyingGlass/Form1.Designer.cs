add78e7f7ed83d650cbe787988d40d5ddd487c37
RongYan Zhang <kawai566@yeah.net>
2017-09-29 14:37:25 +0800

    init

---
+++ MagnifyingGlass/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MagnifyingGlass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip2;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(165, 80);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(164, 38);
+            this.toolStripMenuItem2.Text = "start";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(164, 38);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 400);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helloToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ MagnifyingGlass/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagnifyingGlass
+{
+    public partial class Form1 : Form
+    {
+
+        //public int width = Screen.PrimaryScreen.Bounds.Width;
+        //public int height = Screen.PrimaryScreen.Bounds.Height;
+
+
+        public int destinationWidth = 700;
+        public int destinationHight = 400;
+
+        public bool isStart = true;
+        public Bitmap btm = null;
+        public Bitmap newbtm = null;
+
+
+        public int timeValue = 4;
+
+        private Thread t;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Start();
+        }
+        private void StartDetecting()
+        {
+            isStart = true;
+            t = new Thread(new ThreadStart(Detect));
+            t.Start();
+        }
+        private void Detect()
+        {
+            while (isStart)
+            {
+                Thread.Sleep(10);
+                this.Invoke(new MyDelegate(DetectAction), isStart);
+            }
+
+        }
+        delegate void MyDelegate(bool iss);//定义委托
+        private void DetectAction(bool iss) {
+            pictureBox1.Image = null;
+            if (btm != null)
+                btm.Dispose();
+            if (newbtm != null)
+                newbtm.Dispose();
+            if (iss)
+            {
+                btm = new Bitmap(destinationWidth, destinationHight);
+
+                using (Graphics g = Graphics.FromImage(btm))
+                {
+                    g.CopyFromScreen(Control.MousePosition.X - destinationWidth / 4, Control.MousePosition.Y - destinationHight / 4, 0, 0, Screen.AllScreens[0].Bounds.Size);
+                    g.Dispose();
+                    //pictureBox1.Image = btm;
+                }
+                int newDW = destinationWidth * timeValue;
+                int newDH = destinationHight * timeValue;
+                newbtm = new Bitmap(newDW, newDH);
+                using (Graphics g2 = Graphics.FromImage(newbtm))
+                {
+                    g2.DrawImage(btm, 0 - (newDW - destinationWidth) / 4, 0 - (newDH - destinationHight) / 4, newDW, newDH);
+                    g2.Dispose();
+                    pictureBox1.Image = newbtm;
+                }
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Exit();
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Start();
+        }
+        ~Form1() {
+            // isStart = false;
+            //t.Join();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+        private void Start() {
+            //判断可有可无，因为目前的代码出现托盘的时候只会为最小化
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                //还原
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.Left = 0;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+            this.Activate();
+            StartDetecting();
+        }
+        private void Exit() {
+            isStart = false;
+            t.Suspend();
+            t.Interrupt();
+            Application.ExitThread();
+            Application.Exit();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exit();
+        }
+    }
+}
